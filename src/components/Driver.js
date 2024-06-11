2b3d8b45bc4f03f449273fb13e87aa014065c2dd
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-06-11 17:30:52 +0530

    Driver Deleted

--- src/components/Driver.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Driver = ({ data }) =>
-	<div
-		style={ { backgroundImage: `url(${ process.env.PUBLIC_URL }${ data.driverImage })` } }
-		className="driver"
-	/>;
-
-export default Driver;
